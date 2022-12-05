77c1d856e9813c41527726cd6efd2232f30d39ed
Hong Yan <homer.yan@gmail.com>
2022-12-05 00:09:34 -0700

    feat: initial commit

---
+++ index.mjs
@@ -0,0 +1,71 @@
+import playwright from 'playwright';
+import 'zx/globals';
+
+const CSS_LOC = './data/styles';
+const REL_CSS_LOC = '../styles';
+const TITLE_MD5 = 'd9328d3f7071730e6db055f1fd5edb31';
+
+const run = async () => {
+  const browser = await playwright.chromium.launch({
+    headless: false // Show the browser.
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://mp.weixin.qq.com/s?src=11&timestamp=1670204845&ver=4207&signature=AT-cxLFs4zB2zK*sXCeCaJw6HW8NQbskuaLOcefgCIm4RtF7xmHujXK8w9iBPb8cmNERNBRdsn*MKWzIAASSAOjf1zRsQ2EwDKkdV9NfN*SlTwy7j5r9PoGHVQG1wG58&new=1');
+  
+  let pageHtml = await page.content();
+  pageHtml = await replaceCSSLinksWithLocalFiles(pageHtml, CSS_LOC, REL_CSS_LOC);
+  pageHtml = await replaceImgLinksWithLocalFiles(pageHtml, `./data/bitsea/${TITLE_MD5}_files`, `./${TITLE_MD5}_files`);
+  
+  await fs.outputFileSync(`./data/bitsea/${TITLE_MD5}.html`, pageHtml);
+  
+  await page.waitForTimeout(1000);
+  await browser.close();
+};
+
+
+async function replaceCSSLinksWithLocalFiles (pageHtml, savePath, linkPath) {
+  let updatedPageHtml = pageHtml;
+
+  const cssLinks = pageHtml.match(/<link.*?rel="stylesheet".*?>/g);
+  for (let i=0; i<cssLinks.length; i++) {
+    const link = cssLinks[i];
+    const url = link.replace(/^.*?href="/, 'https:')
+      .replace(/".*$/, '')
+      .replace(/https:https:/, 'https:');
+    // console.log(`${link}\n${url}`);
+
+    const fileName = url.substring(url.lastIndexOf('/')+1);
+    if (!fs.pathExistsSync(`${savePath}/${fileName}`)) {
+      await $`mkdir -p ${savePath} && cd ${savePath} && wget ${url} -q`;
+    }
+
+    updatedPageHtml = updatedPageHtml
+      .replace(link, `<link href="${linkPath}/${fileName}" rel="stylesheet">`);
+  }
+  
+  return updatedPageHtml;
+};
+
+
+async function replaceImgLinksWithLocalFiles (pageHtml, savePath, linkPath) {
+  let updatedPageHtml = pageHtml
+    .replace(/<span class="js_img_placeholder wx_widget_placeholder".*?<\/span><\/span><\/span>/g, '');;
+
+  const imgLinks = pageHtml.match(/<img class="rich_pages.*?>/g);
+  for (let i=0; i<imgLinks.length; i++) {
+    const link = imgLinks[i];
+    const url = link.replace(/^.*?data-src="/, '').replace(/".*>/, '');
+    const fileFormat = url.substring(url.lastIndexOf('wx_fmt=')+7);
+    const urlWithoutFormat = url.replace(url.substring(url.lastIndexOf('/')), '');
+    const fileName = urlWithoutFormat
+      .substring(urlWithoutFormat.lastIndexOf('/')+1) + '.' + fileFormat;
+    await $`mkdir -p ${savePath} && cd ${savePath} && rm -f ${fileName} && wget -O ${fileName} ${url} -q`;
+
+    updatedPageHtml = updatedPageHtml.replace(link, `<img src="${linkPath}/${fileName}">`);
+  };
+  return updatedPageHtml;
+};
+
+
+run();
---
+++ sogou-to-wx.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -z "$1" ] ; then
+  echo 'sogou_link missing.'
+  exit 1
+fi
+SOGOU_LINK=$1
+
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+  MD5_CMD=md5
+fi
+if [[ "$OSTYPE" =~ ^linux ]]; then
+  MD5_CMD=md5sum
+fi
+RANDOM_STR=`echo $RANDOM | $MD5_CMD | head -c 20`
+
+# Fetch Sogou Video's page to get SUV and SNUID cookies
+TMP_COOKIE_FILE=/tmp/$RANDOM_STR-cookie.txt
+curl -sS --cookie-jar $TMP_COOKIE_FILE 'https://v.sogou.com/' > /dev/null
+SUV=`cat $TMP_COOKIE_FILE | grep SUV | awk '{print $7}'`
+SNUID=`cat $TMP_COOKIE_FILE | grep SNUID | awk '{print $7}'`
+rm -f $TMP_COOKIE_FILE
+
+TMP_REDIR_FILE=/tmp/$RANDOM_STR-redir.html
+curl -sS --cookie "SUV=$SUV" --cookie "SNUID=$SNUID" $SOGOU_LINK > $TMP_REDIR_FILE
+cat $TMP_REDIR_FILE | grep 'url +=' | grep -Eo \'.*?\' | sed 's/'\''//g' | tr -d '\n' | sed 's/http:/https:/'
+rm -f $TMP_REDIR_FILE
+echo
