552cccc6e6eaaff2b276d90dc8121b241336b681
Hong Yan <homer.yan@gmail.com>
2022-12-08 00:50:34 -0700

    feat: generate artile list page

--- index.mjs
+++ index.mjs
@@ -1,24 +1,24 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import 'zx/globals';
-import { fetchWithProxy } from './src/util.mjs';
+import { fetchWebPageContent, generateIndexHtml } from './src/util.mjs';
 import { extractTitle, extractTitleChecksum, extractWxPubAccountArticleUrl } from './src/sogou-result.mjs';
 import { saveWeixinArticle } from './src/weixin-article.mjs';
 import defaults from './defaults.json' assert { type: 'json' };
 
 const PUB_ACCOUNT = process.env.PUB_ACCOUNT || defaults.PUB_ACCOUNT;
-const ARTICLE_BASE = process.env.SAVED_ARTICLE_LOCATION || defaults.SAVED_ARTICLE_LOCATION;
+const ARTICLE_SAVE_LOCATION = process.env.ARTICLE_SAVE_LOCATION || defaults.ARTICLE_SAVE_LOCATION;
 const SOGOU_WX_QUERY_BASE = process.env.SOGOU_WX_QUERY_BASE || defaults.SOGOU_WX_QUERY_BASE;
 
 
 const run = async () => {
   const accounts = fs.readJsonSync(PUB_ACCOUNT);
-
+/*
   // for (let i=1; i<accounts.length; i++) {
   for (let i=0; i<accounts.length; i++) {
     const sogouQueryUrl = SOGOU_WX_QUERY_BASE + accounts[i].wx_pub_account_id;
-    const pageHtml = await fetchWithProxy(sogouQueryUrl);
-    if (pageHtml.length === 0) {
+    const pageHtml = await fetchWebPageContent(sogouQueryUrl);
+    if (!pageHtml) {
       continue;
     }
     
@@ -37,11 +37,14 @@ const run = async () => {
     }
     
     const articleUrl = await extractWxPubAccountArticleUrl(anchorElement);
-    await saveWeixinArticle(articleUrl, accounts[i].wx_pub_account_id, ARTICLE_BASE, checksum);
+    await saveWeixinArticle(articleUrl, accounts[i].wx_pub_account_id, ARTICLE_SAVE_LOCATION, checksum);
     accounts[i].latest_article_md5 = extractTitleChecksum(anchorElement);
   }
-
+*/
   fs.writeJsonSync(PUB_ACCOUNT, accounts, { spaces: "  " });
+  const articleListPage = generateIndexHtml(ARTICLE_SAVE_LOCATION, 
+      accounts.map(account => account.wx_pub_account_id));
+  fs.outputFileSync(`${ARTICLE_SAVE_LOCATION}/index.html`, articleListPage);
 };
 
 
--- src/util.mjs
+++ src/util.mjs
@@ -7,8 +7,8 @@ const PROXY_POOL = process.env.PROXY_POOL || defaults.PROXY_POOL;
 const CONNECT_TIMEOUT = process.env.CONNECT_TIMEOUT || defaults.CONNECT_TIMEOUT;
 const RETRY_COUNT = process.env.RETRY_COUNT || defaults.RETRY_COUNT;
 
-export async function fetchWithProxy(url) {
-  let pageHtml = await downloadWithOptionalProxy(url, null);
+export async function fetchWebPageContent(url) {
+  let pageHtml = await downloadWithOptionalProxy(url);
   if (pageHtml && pageHtml.length >= 200) {
     return pageHtml;
   }
@@ -95,4 +95,89 @@ async function downloadWithOptionalProxy(url, proxy = null) {
   }
 
   return pageHtml;
+}
+
+
+export function generateIndexHtml(path, subfolders) {
+  const htmlFiles = [];
+
+  subfolders.forEach(subfolder => {
+    const filenames = fs.readdirSync(`${path}/${subfolder}`);
+    filenames.filter(file => file.endsWith('.html'))
+      .forEach(file => { htmlFiles.push(`./${subfolder}/${file}`) });
+  });
+  
+  const template = getIndexTemplate(path);
+  let anchorElements = '';
+  htmlFiles.forEach(file => {
+    anchorElements += generateArticleLink(path, file);
+  });
+  
+  return template.replace(`id="wx_article_list">`, `id="wx_article_list">${anchorElements}`);
+}
+
+
+function getIndexTemplate(path) {
+  let template = '';
+  try {
+    template = fs.readFileSync(`${path}/template.html`, { encoding:'utf8', flag:'r' });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    if (!template.match(/id="wx_article_list"/g)) {
+      // Default template if templdate.html is not present.
+      template = `
+        <!doctype html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+              rel="stylesheet"
+              integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+              crossorigin="anonymous">
+            <title>Articles</title>
+            <style>a { display: inline; }</style>
+          </head>
+          <body>
+            <div class="container-fluid px-0">
+              <p><h2 align="center">Articles</h2></p>
+              <div class="list-group list-group-flush" id="wx_article_list">
+                <!-- <a href="LINK_OF_ARTICLE" class="list-group-item list-group-item-action">TITLE_OF_ARTICLE</a> -->
+              </div>
+              <p>&nbsp;</p>
+            </div>
+          </body>
+        </html>
+      `;
+    };
+  }
+  
+  return template;
+}
+
+
+function generateArticleLink(path, filename) {
+  let anchorElement = '';
+
+  try {
+    const articleHtml = fs.readFileSync(`${path}/${filename}`, { encoding:'utf8', flag:'r' });
+    const title = articleHtml.match(/<meta property="og:title" content="(.*?)"/)[1];
+    const author = articleHtml.match(/<meta name="author" content="(.*?)"/)[1];
+    const description = articleHtml.match(/<meta name="description" content="(.*?)"/)[1];
+    anchorElement = `
+      <a href="${filename}" class="list-group-item list-group-item-action">
+        ${author} - ${title}<br>
+        ${description}
+      </a>
+    `;
+  } catch (error) {
+    anchorElement = `
+      <a href="${filename}" class="list-group-item list-group-item-action">
+        ${filename}
+      </a>
+    `;
+  }
+
+  return anchorElement;
 }
